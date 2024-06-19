41a4fa72aab84d9c17c89f1533997b97a8851d94
Asthanaji05 <shashank.asthana05@gmail.com>
2024-06-19 19:21:12 +0530

    second Commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-<<<<<<< HEAD
-export default defineConfig({
-  build: '/notes/',
-  plugins: [react()],
-=======
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
@@ -15,9 +10,8 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = '/notes/'
+    config.base = '/NotesFullStack/'
   }
 
   return config
->>>>>>> a7516a912ed491a9f8c947bb93054739621aaaa1
 })
